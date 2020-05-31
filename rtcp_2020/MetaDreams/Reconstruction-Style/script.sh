cc7510c98c108be138c045eee0719065c78f737d
akira2357 <martino.n.bernasconi@gmail.com>
2020-05-31 15:00:14 +0200

    create RecustructStyle

---
+++ rtcp_2020/MetaDreams/Reconstruction-Style/script.sh
@@ -0,0 +1,7 @@
+python3 src/main.py test \
+    --content-image images/content-images/white.jpg \
+    --output-image images/output-images/white_Dream.jpg \
+    --model Dream.pth \
+    --content-size 256 \
+    --original-colors 0 \
+    --cuda 0 \
